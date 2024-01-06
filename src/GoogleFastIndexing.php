56ceac4ada920e442dbab517784e298c53d5c636
dllpl <nick.iv.dev@gmail.com>
2024-01-06 21:27:11 +0300

    Убрал googleapis

--- src/GoogleFastIndexing.php
+++ src/GoogleFastIndexing.php
@@ -1,59 +1,58 @@
 <?php
 
-use Google\Client;
-use Google\Service\Indexing;
+use Firebase\JWT\JWT;
+use GuzzleHttp\Client;
 
 final class GoogleFastIndexing
 {
     private Client $client;
 
+    private string $jwt;
+
     /**
-     * @param string $keyPath json файл из личного аккаунта разработчика Google
+     * @param string $keyFile json файл из личного аккаунта разработчика Google
      * @throws \Google\Exception
      */
-    public function __construct(string $keyPath)
+    public function __construct(string $keyFile)
     {
-        /** @var string keyPath */
-        $this->keyPath = $keyPath;
+        $key = json_decode(file_get_contents($keyFile), true);
 
-        $this->client = new Client();
-        $this->client->setAuthConfig($keyPath);
-        $this->client->addScope('https://www.googleapis.com/auth/indexing');
+        $this->jwt = JWT::encode([
+            'iss' => $key['client_email'],
+            'scope' => 'https://www.googleapis.com/auth/indexing',
+            'aud' => 'https://oauth2.googleapis.com/token',
+            'exp' => time() + 3600,
+            'iat' => time(),
+        ], $key['private_key'], 'RS256');
     }
 
-    /**
-     * @param string $urlsPath
-     * @return Indexing\PublishUrlNotificationResponse
-     */
-    public function send(string $urlsPath): Indexing\PublishUrlNotificationResponse|string
+    public function send(string $batchFile)
     {
-        $service = new Indexing($this->client);
+        $batch = explode("\n", file_get_contents($batchFile));
 
-        $batch = explode("\n", file_get_contents($urlsPath));
+        $items = array_map(function ($line) {
+            return [
+                'Content-Type' => 'application/http',
+                'Content-ID' => '',
+                'body' =>
+                    "POST /v3/urlNotifications:publish HTTP/1.1\n" .
+                    "Content-Type: application/json\n\n" .
+                    json_encode([
+                        'url' => $line,
+                        'type' => 'URL_UPDATED'
+                    ])
+            ];
+        }, $batch);
 
-        try {
-            $this->client->fetchAccessTokenWithAssertion();
+        $client = new Client();
+        $response = $client->request('POST', 'https://indexing.googleapis.com/batch', [
+            'headers' => [
+                'Content-Type' => 'multipart/mixed',
+                'Authorization' => 'Bearer ' . $this->jwt
+            ],
+            'json' => $items
+        ]);
 
-            $items = array_map(function($line) {
-                return [
-                    'Content-Type' => 'application/http',
-                    'Content-ID' => '',
-                    'body' =>
-                        "POST /v3/urlNotifications:publish HTTP/1.1\n" .
-                        "Content-Type: application/json\n\n" .
-                        json_encode([
-                            'url' => $line,
-                            'type' => 'URL_UPDATED'
-                        ])
-                ];
-            }, $batch);
-
-            return $service->urlNotifications->publish([
-                'body' => ['urlNotification' => $items]
-            ]);
-
-        } catch (Exception $e) {
-            return 'Ошибка: ' . $e->getMessage();
-        }
+        echo $response->getBody()->getContents();
     }
 }
